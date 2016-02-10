2486b62e35cb92f54a879b46f9ebf2e237587b85
omarram <omarramadneh@gmail.com>
2016-02-10 21:43:18 +0200

    Added both product view and basket features

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   mount Shoppe::Engine => "/shoppe"
+  get "product/:permalink", to: "products#show", as: "product"
+  post "product/:permalink", to: "products#buy", as: "buy"
+  get "basket", to: "orders#show"
+  delete "basket", to: "orders#destroy"
+  root to: "products#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
